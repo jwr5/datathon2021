d163c02731dab4f1c78ab10c37979cb01b34430e
mahoganu <38735727+mahoganu@users.noreply.github.com>
2021-02-06 12:26:53 -0600

    deleted seaborn

--- parse.py
+++ parse.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
-
 def parse_tickers_names(filename):
     df = pd.read_csv(filename, delimiter=',')
     exchange_list = df[['Symbol', 'Name']]
     return exchange_list
-
 
 def df_concat(filenames):
     ticks_and_names = pd.DataFrame()
@@ -16,13 +14,11 @@ def df_concat(filenames):
         ticks_and_names = pd.concat([ticks_and_names, exchange_temp])
     return ticks_and_names
 
-
 def df_cols_to_list(filenames):
     ticks_and_names = df_concat(filenames)
     ticks = ticks_and_names['Symbol'].tolist()
     companies = ticks_and_names['Name'].tolist()
     return ticks, companies
-
 
 def company_pruner(company_list, cutoff_frequency):
     pruned_list = []
@@ -32,23 +28,21 @@ def company_pruner(company_list, cutoff_frequency):
     for company_name in split_list:
         for word in company_name:
             word_freq[word] += 1
-
+    
     for word, freq in word_freq.items():
         for company_name in split_list:
             if (word in company_name) and (freq >= cutoff_frequency):
                 company_name.remove(word)
-
+    
     for company_name in split_list:
         comp_string = ' '.join([str(word) for word in company_name])
         pruned_list.append(comp_string)
 
     return pruned_list
 
-
 def zip_list(list1, list2):
     zip_list = list(zip(list1, list2))
     return zip_list
-
 
 def pruner_bigboy():
     ticks, companies = df_cols_to_list(['amex.csv', 'nyse.csv', 'nasdaq.csv'])
@@ -56,7 +50,7 @@ def pruner_bigboy():
     ticks_names = zip_list(ticks, pruned_companies)
     tn_copy = ticks_names.copy()
     for item in ticks_names:
-        if item[1] == "" or item[1] == " ":
+        if item[1] == "" or item [1] == " ":
             tn_copy.remove(item)
-
+    
     return tn_copy
